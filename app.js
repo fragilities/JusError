5374ce97cbee68249bf97cad5b66f2009f9837a2
Zoe <zoezachary27@gmail.com>
2019-11-20 10:19:08 +0700

    test

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict'
 
 const express = require('express')
@@ -22,18 +21,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, ()=>{
   console.log(`App listening on port ${PORT}`);
 })
-=======
-const express = require('express')
-const app = express()
-const PORT = 3000
-
-const JuiceRouter = require('./routers/JuiceRouter')
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
-
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: true}))
-
-app.use('/juice', JuiceRouter)
-
->>>>>>> 8c097f9a96e199677f1a75c80762aa0179225397
