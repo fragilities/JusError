54da319530da1eed3c2643e78ca5195a1754833c
Zoe <zoezachary27@gmail.com>
2019-11-20 14:18:16 +0700

    juice add and the function to add ingredients into the conjunction table are finished. however, there is no validation yet.

--- controllers/JuiceController.js
+++ controllers/JuiceController.js
@@ -1,6 +1,7 @@
 const Juice = require('../models').Juice
 const Ingredient = require('../models').Ingredient
 const UserJuice = require('../models').UserJuice
+const IngredientJuice = require('../models').IngredientJuice
 
 class JuiceController {
 
@@ -16,16 +17,43 @@ class JuiceController {
   }
 
   static addPage(req, res) {
-
     const messages = {}
     if(req.query.error) messages.error = req.query.error
 
-    res.render('juiceAdd', {messages})
+    Ingredient.findAll()
+    .then(ingredients => res.render('juice/add', {ingredients, messages}))
+    .catch(err => res.send(err.message))
   }
 
   static add(req, res) {
 
+    let ingredients = []
+    
+    for(let key in req.body) {
+      let ingredientTemp = key.split('-')
+      if(ingredientTemp.length > 1) {
+        if(ingredientTemp[1] == 'radio') {
+          if(req.body[key] == 1) {
+            let amountKey = ingredientTemp[0] + '-amount'
+            ingredients.push([req.body[ingredientTemp[0]], req.body[amountKey]])
+          }
+        }
+      }
+    }
+
     Juice.create(req.body)
+    .then(juice => {
+
+      let conjunctionPromises = []
+
+      for(let i in ingredients) conjunctionPromises.push(IngredientJuice.create({
+        IngredientId: ingredients[i][0],
+        JuiceId: juice.id,
+        amount: ingredients[i][1]
+      }))
+      
+      return Promise.all(conjunctionPromises)
+    })
     .then(() => res.redirect(`/juice?success=Resep jus telah berhasil dibuat`))
     .catch(err => res.redirect(`/juice/add?error=${err.message}`))
   }
@@ -35,8 +63,15 @@ class JuiceController {
     const messages = {}
     if(req.query.error) messages.error = req.query.error
 
+    let tempJuice
+
     Juice.findByPk({where: {id: req.params.id}})
-    .then(juice => res.render('juiceEdit', {juice, messages}))
+    .then(juice => {
+
+      tempJuice = juice
+      return Ingredient.findAll()
+    })
+    .then(ingredients => res.render('juiceEdit', {tempJuice, ingredients, messages}))
     .catch(err => res.redirect(`/juice?error=${err.message}`))
   }
 
