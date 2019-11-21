93c3b3cf323ca22411939b376e5df7dbe6124c14
Zoe <zoezachary27@gmail.com>
2019-11-21 23:01:37 +0700

    my commit

--- routes/ingredient.js
+++ routes/ingredient.js
@@ -3,6 +3,8 @@
 const router = require('express').Router();
 const IngredientController = require('../controllers/ingredientController');
 
+router.get('/getRaw', IngredientController.raw)
 
+router.get('/', IngredientController.all)
 
 module.exports = router;
